c6ecc08f69039acc0120160330bed7d8f917ee4c
rickynero02 <riccardonisdi2002@libero.it>
2022-08-24 17:01:56 +0200

    modified player

--- entities/player.cpp
+++ entities/player.cpp
@@ -2,50 +2,17 @@
 #include <ncurses.h>
 #include "player.hpp"
 
-Player::Player(){   //aspetto giocatore
-    max_health = 20;  //massima vita giocatore
-    health = max_health;
-    damage = 2;      //danno
+Player::Player(int x, int y) : Entity() {
+    position.x = x;
+    position.y = y;
+    health = 20;
+    max_health = 20;
+    damage = 2;
+    icon = 'P';
     bullet_icon = '-';
-    win = newwin(1,1,0,0);
-    mvwprintw(win, 0, 0, "%s", icon);
-    wrefresh(win);
+    win = newwin(1, 1, 0, 0);
 }
 
-Player::Player(int x, int y){     
-    max_health = 20;  
-    health = max_health;
-    current_position.x = x;
-    current_position.y = y;
-}
+void Player::move(Position pos) {
 
-void Player::updateHealth(int n){
-    health = health+n;
-    if(health > max_health){
-        max_health = health;
-    }
-}
-
-void Player::setHealth(int n){
-    health = n;
-}
-
-int Player::getHealth(){
-    return health;
-}
-
-void Player::setDamage(int n){
-    damage = n;
-}
-
-int Player::getDamage(){
-    return damage;
-}
-
-void Player::setBulletIcon(char n){
-    bullet_icon = n;
-}
-
-char Player::getBulletIcon(){
-    return bullet_icon;
 }
