bcc9a0097b4498296353e5b4a42e8c1489903692
fvckbog02 <bngsquod@gmail.com>
2022-08-25 12:58:57 +0200

    inserimento porta aperta, correzione bug

--- maps/map.cpp
+++ maps/map.cpp
@@ -13,20 +13,42 @@ Map::Map() {
 void Map::createWalls() {
     srand(time(NULL));      
 
-    int m = rand()%20+10;
-    for(int i = 1; i <= 50; i++){
+    int m = rand()%10+11;
+    for(int i = 1; i <= 40; i++){
         mvwprintw(win, m, i, "%c", '-');          
     }
 
-    int n = rand()%60+10;
-    for(int i = 1; i <= 15; i++){
-        mvwprintw(win, i, n, "%c", '|');
+    if(m<=11){
+        for(int i = 1; i <= 10; i++){
+        mvwprintw(win, i, 60, "%c", '|');
+        }
     }
-}
+
+    else{
+        int n = rand()%25+25;
+        if(n<=19){
+        for(int i = 1; i <= 10; i++){
+        mvwprintw(win, i, 23, "%c", '|');
+                } 
+            }
+        else{
+                for(int i = 1; i <= 10; i++){
+                mvwprintw(win, i, n, "%c", '|');
+                }
+            }
+        }
+       
+    }
 
 void Map::createDoors() {
     mvwprintw(win, 0, 10, "XXXXXXXXX");                                        
-    //mvwprintw(win, 10, 0, "         ");    
+    
+    for(int i=10; i<=19; i++){
+
+     mvwprintw(win, 29, i, " ");  
+    
+    }
+     
 }
 
 Map::~Map() { 
