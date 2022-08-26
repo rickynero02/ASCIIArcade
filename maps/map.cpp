773c8b24657f0fe71b0c8deedc93def0b1883b54
fvckbog02 <bngsquod@gmail.com>
2022-08-26 03:26:43 +0200

    aggiunta 4 mappe nuove, con controllo bug.

--- maps/map.cpp
+++ maps/map.cpp
@@ -1,6 +1,7 @@
 #include "map.hpp"
 #include <ctime>
 #include <cstdlib>
+#include <ncurses.h>
 
 Map::Map() { 
     win = newwin(height, width, start_y, start_x);
@@ -14,11 +15,40 @@ void Map::createWalls() {
     srand(time(NULL));      
 
     int m = rand()%10+11;
-    for(int i = 1; i <= 40; i++){
-        mvwprintw(win, m, i, "%c", '-');          
+
+    int m2 = rand()%10+11;
+
+    int s = rand()%2+10;
+
+    bool b;
+
+
+    if(s%2==0){
+        b= true;
+    }
+    else{
+        b= false;
     }
 
-    if(m<=11){
+
+    if(b==true){        // OStacoli orizzontale Sinistra e Sopra   //13
+
+
+    if(m<=13){          //evita incontri con mura verticali
+        for(int i = 1; i <= 40; i++){
+        mvwprintw(win, 16, i, "%c", '-');
+    }
+    }
+
+    else{
+        for(int i = 1; i <= 40; i++){
+        mvwprintw(win, m, i, "%c", '-');          
+
+        }
+    }
+
+
+    if(m<=11){                              //non va contro porta chiusa
         for(int i = 1; i <= 10; i++){
         mvwprintw(win, i, 60, "%c", '|');
         }
@@ -37,8 +67,97 @@ void Map::createWalls() {
                 }
             }
         }
-       
+
     }
+    
+    else{               // Ostacoli Orizzontale Destra e Orizzontale Sinistra ; Ostacolo QUADRATO ; ostacolo linea larga
+
+
+
+        if(m+m2<=28){
+
+        for(int i = 15; i <= 55; i++){     //sotto
+        
+        mvwprintw(win, 23, i, "%c", '-');
+        } 
+
+        for(int i = 7; i <= 22; i++){
+        
+        mvwprintw(win, i, 14, "%c", '|');
+        }
+        
+        
+        
+        for(int i = 7; i <= 22; i++){
+        
+        mvwprintw(win, i, 56, "%c", '|');
+        }  
+
+
+        for(int i = 1; i <=13; i++){     //ostacolo lato sinistro quadrato 
+        
+        mvwprintw(win, 12, i, "%c", '-');
+        } 
+
+
+
+
+
+        for(int i = 15; i <= 55; i++){        //sopra
+        
+        mvwprintw(win, 6, i, "%c", '-');
+        } 
+        
+    
+    }
+
+    else if(m-m2<0){                             //ostacolo linea larga orizzontale
+
+        for(int i = 1; i <= 62; i++){     
+        
+        mvwprintw(win, 15, i, "%c", '-');
+        } 
+
+    }
+
+    else if(m+m2>=38){                             //ostacolo linea larga verticale
+
+        for(int i = 1; i <= 22; i++){     
+        
+        mvwprintw(win, i, 35, "%c", '|');
+        } 
+
+    }
+
+
+    else{
+        
+        for(int i = 68; i >= 29; i--){
+        mvwprintw(win, m, i, "%c", '-');
+        }
+
+        if(m==m2 or m==m2+1 or m==m2+2 or m==m2+3 or m==m2+4 or m==m2+5 or m==m2-1 or m==m2-2 or m==m2-3 or m==m2-4 or m==m2-5){
+        for(int i = 1; i <= 40; i++){
+        mvwprintw(win, 25, i, "%c", '-'); 
+                }
+        }
+        
+        else{
+
+        for(int i = 1; i <= 40; i++){
+        mvwprintw(win, m2, i, "%c", '-'); 
+            }
+
+        }
+    }
+
+    }
+
+}
+
+
+
+
 
 void Map::createDoors() {
     mvwprintw(win, 0, 10, "XXXXXXXXX");                                        
@@ -52,5 +171,5 @@ void Map::createDoors() {
 }
 
 Map::~Map() { 
-    delwin(win);    //distruttore di memoria allocata 
+    delwin(win);
 }
